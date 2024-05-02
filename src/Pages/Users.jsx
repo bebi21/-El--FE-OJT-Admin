72ccabff99ce106c7f0dec8b2f1d48229e968df1
thanhgit123 <hai880345@gmail.com>
2024-05-02 15:44:48 +0700

    FE pagnigation user

--- src/Pages/Users.jsx
+++ src/Pages/Users.jsx
@@ -1,69 +1,11 @@
 import { Modal, Pagination, Radio, notification } from "antd";
 import { useEffect, useState } from "react";
 import publicAxios from "../database/publicAxios";
-
-
-//   {
-//     id: 1,
-//     fullname: "loc",
-//     phone: "0123456789",
-//     create_date: "2022-07-01",
-//     active: 1,
-//   },
-//   {
-//     id: 2,
-//     fullname: "loc",
-//     phone: "0123456789",
-//     create_date: "2022-07-01",
-//     active: null,
-//   },
-//   {
-//     id: 1,
-//     fullname: "loc",
-//     phone: "0123456789",
-//     create_date: "2022-07-01",
-//     active: 1,
-//   },
-//   {
-//     id: 2,
-//     fullname: "loc",
-//     phone: "0123456789",
-//     create_date: "2022-07-01",
-//     active: null,
-//   },
-//   {
-//     id: 1,
-//     fullname: "loc1",
-//     phone: "0123456789",
-//     create_date: "2022-07-01",
-//     active: null,
-//   },
-//   {
-//     id: 2,
-//     fullname: "loc2",
-//     phone: "0123456789",
-//     create_date: "2022-07-01",
-//     active: 1,
-//   },
-//   {
-//     id: 1,
-//     fullname: "lo3c",
-//     phone: "0123456789",
-//     create_date: "2022-07-01",
-//     active: 1,
-//   },
-//   {
-//     id: 2,
-//     fullname: "lo4c",
-//     phone: "0123456789",
-//     create_date: "2022-07-01",
-//     active: 1,
-//   },
-// ];
 const TableUser = () => {
   const [isModalOpen, setIsModalOpen] = useState(false);
   const [status, setStatus] = useState();
   const [api, contextHolder] = notification.useNotification();
+  const [totalPage, setTotalPage] = useState(0);
   const openNotificationWithIcon = () => {
     api.success({
       message: "Thành Công",
@@ -73,13 +15,6 @@ const TableUser = () => {
   };
   /* call api  */
   const [brandData, setBrandData] = useState([]);
-  const takeDataInDb = async () => {
-    const response = await publicAxios.get("/users/getAllUser");
-    setBrandData(response.data);
-  };
-  useEffect(() => {
-    takeDataInDb();
-  }, []);
   const showModal = (item) => {
     setStatus(item);
     setIsModalOpen(true);
@@ -90,10 +25,14 @@ const TableUser = () => {
       id: status?.id,
     };
     const response = await publicAxios.put("/users/updateStatus/", data);
-    takeDataInDb();
+    handlePaginationRenderOne();
     openNotificationWithIcon();
     setIsModalOpen(false);
-  }
+  };
+
+  useEffect(() => {
+    handlePaginationRenderOne();
+  }, []);
   const handleCancel = () => {
     setStatus("");
     setIsModalOpen(false);
@@ -101,22 +40,33 @@ const TableUser = () => {
   const onChange = (e) => {
     setStatus({ ...status, change_active: e.target.value });
   };
-  /* phân trang */
-  const [currentPage, setCurrentPage] = useState(1);
-  const itemsPerPage = 4; // số lượng items mỗi trang
-  // Tạo một state mới để lưu trữ dữ liệu hiển thị trên trang hiện tại
-  const [currentBrandData, setCurrentBrandData] = useState([]);
-  // Hàm này sẽ được gọi mỗi khi trang thay đổi
-  // useEffect(() => {
-  //   const indexOfLastItem = currentPage * itemsPerPage;
-  //   const indexOfFirstItem = indexOfLastItem - itemsPerPage;
-  //   setCurrentBrandData(brandData.slice(indexOfFirstItem, indexOfLastItem));
-  // }, [currentPage, itemsPerPage]);
 
-  // Hàm này sẽ được gọi khi người dùng thay đổi trang
-  const handlePageChange = (page) => {
-    setCurrentPage(page);
+  const handlePaginationRenderOne = async () => {
+    const limit = 4;
+    let firstPage = 1;
+    try {
+      const response = await publicAxios.get(
+        `/users/paginationUser?page=${firstPage}&limit=${limit}`
+      );
+      setBrandData(response.data);
+      setTotalPage(response.data.totalItem);
+    } catch (error) {
+      console.log(error);
+    }
   };
+  const handlePagination = async (page) => {
+    const limit = 4;
+    window.scrollTo({ top: 600, behavior: "smooth" });
+    try {
+      const response = await publicAxios.get(
+        `/users/paginationUser?page=${page}&limit=${limit}`
+      );
+      setBrandData(response.data);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
   return (
     <>
       {contextHolder}
@@ -141,8 +91,7 @@ const TableUser = () => {
       <div className=" rounded-sm border border-stroke bg-white px-5 pt-6 pb-2.5 shadow-default dark:border-strokedark dark:bg-boxdark sm:px-7.5 xl:pb-1">
         <h4 className="mb-6 text-xl font-semibold text-black dark:text-white">
           Quản Lý Người Dùng
-        </h4>
-
+        </h4> 
         <div className="flex flex-col h-[68vh] relative ">
           <div className="py-[10px] grid grid-cols-3 rounded-sm bg-gray-2 dark:bg-meta-4 sm:grid-cols-6">
             <div className="p-2.5  text-center xl:p-2">
@@ -176,63 +125,59 @@ const TableUser = () => {
               </h5>
             </div>
           </div>
-          {
-            brandData.map((brand, key) => (
-              <div
-                className={
-                  "grid grid-cols-3 sm:grid-cols-6 text-[14px] hover:bg-slate-50"
-                }
-                key={key}
-              >
-                <div className="flex justify-center items-center p-2.5 ">
-                  <p className="hidden text-black  dark:text-white sm:block">
-                    {key + 1}
-                  </p>
-                </div>
-                <div className="flex justify-center items-center p-2.5 mr-[27px]">
-                  <p className="hidden text-black  dark:text-white sm:block">
-                    {brand.full_name}
-                  </p>
-                </div>
-                <div className="flex items-center justify-start p-2.5 xl:p-8">
-                  <p className="text-black dark:text-white">{brand.phone}</p>
-                </div>
-
-                <div className="flex items-center justify-center p-2.5 xl:p-5">
-                  <p className="text-meta-3">{brand.create_date}</p>
-                </div>
-                <div className="hidden items-center justify-center p-2.5 sm:flex xl:p-5">
-                  {brand.is_active === true ? (
-                    <>
-                      {" "}
-                      <p className="text-green-600 font-bold">Đang Hoạt Động</p>
-                    </>
-                  ) : (
-                    <>
-                      {" "}
-                      <p className="text-red-600 font-bold  ">
-                        Ngừng Hoạt Động
-                      </p>
-                    </>
-                  )}
-                </div>
-                <div className="  flex items-center justify-start ml-[40px]">
-                  <p
-                    onClick={() => showModal(brand)}
-                    className="text-black cursor-pointer px-[20px] py-[7px] rounded-md hover:bg-slate-500 hover:text-white bg-slate-200"
-                  >
-                    Chỉnh Sửa
-                  </p>
-                </div>
+          {brandData.data?.map((brand, key) => (
+            <div
+              className={
+                "grid grid-cols-3 sm:grid-cols-6 text-[14px] hover:bg-slate-50"
+              }
+              key={key}
+            >
+              <div className="flex justify-center items-center p-2.5 ">
+                <p className="hidden text-black  dark:text-white sm:block">
+                  {key + 1}
+                </p>
               </div>
-            ))}
-          <div className="absolute flex justify-center left-[25%] bottom-[40px]">
+              <div className="flex justify-center items-center p-2.5 mr-[27px]">
+                <p className="hidden text-black  dark:text-white sm:block">
+                  {brand.full_name}
+                </p>
+              </div>
+              <div className="flex items-center justify-start p-2.5 xl:p-8">
+                <p className="text-black dark:text-white">{brand.phone}</p>
+              </div>
+
+              <div className="flex items-center justify-center p-2.5 xl:p-5">
+                <p className="text-meta-3">{brand.create_date}</p>
+              </div>
+              <div className="hidden items-center justify-center p-2.5 sm:flex xl:p-5">
+                {brand.is_active === true ? (
+                  <>
+                    {" "}
+                    <p className="text-green-600 font-bold">Đang Hoạt Động</p>
+                  </>
+                ) : (
+                  <>
+                    {" "}
+                    <p className="text-red-600 font-bold  ">Ngừng Hoạt Động</p>
+                  </>
+                )}
+              </div>
+              <div className="  flex items-center justify-start ml-[40px]">
+                <p
+                  onClick={() => showModal(brand)}
+                  className="text-black cursor-pointer px-[20px] py-[7px] rounded-md hover:bg-slate-500 hover:text-white bg-slate-200"
+                >
+                  Chỉnh Sửa
+                </p>
+              </div>
+            </div>
+          ))}
+          <div className="absolute flex justify-center left-[35%] bottom-[40px]">
             <Pagination
-              showQuickJumper
-              current={currentPage}
-              onChange={handlePageChange}
-              pageSize={itemsPerPage}
-              total={500}
+              onChange={handlePagination}
+              defaultCurrent={1}
+              pageSize={brandData?.itemByPage}
+              total={totalPage}
               showSizeChanger={false}
             />
           </div>
--- src/api/course/index.js
+++ src/api/course/index.js
@@ -25,7 +25,7 @@ export const handleSearchCourseApi = async (dataValue) => {
     return error;
   }
 };
-S;
+
 
 export const handlePaginationRenderOneApi = async (firstPage, limit) => {
   try {
